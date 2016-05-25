6942b86288b80a7369da9df7d7862451fb7a3535
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-05-25 11:26:02 +0300

    init

---
+++ DTest3/main.d
@@ -0,0 +1,36 @@
+﻿module main;
+
+import std.random;
+import std.stdio;
+import std.datetime;
+
+int main() {
+	int[] source;
+	source.length = 50_000_000;
+
+	foreach(ref x; source)
+		x = uniform(0, int.max);
+
+	int[int] m;
+
+	auto sw = StopWatch(AutoStart.yes);
+	foreach(x; source) m[x] = 0;
+	sw.stop();
+	writefln("Insertion: %d msecs\n", sw.peek.msecs);
+	writefln("Map.length = %d", m.length);
+
+	sw.reset();
+	sw.start();
+	auto acc = 0;
+	foreach(x; source) {
+		auto v = x in m;
+		if (v !is null)
+			acc += *v % 10;
+		auto _ = m[x];
+	}
+	sw.stop();
+	writefln("Lookup: %d msecs\n", sw.peek.msecs);
+
+	readln();
+	return 0;
+}
